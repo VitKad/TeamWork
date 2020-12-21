1f0275a39b49ac5a7965fb905b2312c8e968ad3c
Semanov Nikita <semanov_ns@mail.ru>
2020-12-21 20:32:18 +0300

    Добавлен класс таблицы расходов

---
+++ TeamWork/ExpenseList.h
@@ -0,0 +1,18 @@
+#ifndef EXPENSELIST_H    
+#define EXPENSELIST_H   
+#include <vector> 
+#include "Expense.h"
+
+class ExpenseList
+{
+private:
+	vector<Expense*> vectPtrsExpenses;
+	vector<Expense*>::iterator iter;
+public:
+	~ExpenseList();
+	void insertExp(Expense*);
+	void display();
+	float displaySummary(); 
+};
+
+#endif 
