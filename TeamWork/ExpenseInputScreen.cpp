522cf70044a0cd5e036647aefceb649586289fd3
Semanov Nikita <semanov_ns@mail.ru>
2020-12-21 22:24:19 +0300

    Добавлен экран ввода расходов и экран ввода логина и пароля

---
+++ TeamWork/ExpenseInputScreen.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h" 
+#include "ExpenseInputScreen.h" 
+ 
+ExpenseInputScreen::ExpenseInputScreen(ExpenseList* per) : ptrExpenseList(per)
+{
+}
+
+void ExpenseInputScreen::setExpense()
+{
+
+	string category, payee, date;
+	float amount;
+	cout << "Введите категорию расходов (Водитель, Материалы): ";
+	getaLine(category);
+	cout << "Введите организацию (ИП Иванов, ОАО Грузоперевозки): ";
+	getaLine(payee);
+	cout << "Введите дату платежа (01.10.2020): ";
+	getaLine(date);
+	cout << "Введите сумму в рублях(500): ";
+	cin >> amount;
+	cin.ignore(80, '\n');  
+	Expense* ptrExpense = new Expense(category, payee, date, amount);   
+	ptrExpenseList->insertExp(ptrExpense);
+}
